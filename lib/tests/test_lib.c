05ebbac63aa480b6f4cad06fbf87ef346e0798cf
alanabispo <alana.bispo@ufba.br>
2022-06-26 21:03:02 -0300

    Adiciona novas funcoes para o test_lib

--- lib/tests/test_lib.c
+++ lib/tests/test_lib.c
@@ -1,3 +1,4 @@
+#pragma ide diagnostic ignored "readability-non-const-parameter"
 #include "test_lib.h"
 
 #include <stdbool.h>
@@ -17,4 +18,42 @@ void cleanBuffer(char *dest, int tam) {
     for (i = 0; i < tam; i++) {
         dest[i] = '\0';
     }
+}
+
+// Verifica se a string possui caracteres estranhos após o \0
+bool assertBufferStr(char *buf, size_t tam_buf, const char *exp_str) {
+    size_t tam_exp = strlen(exp_str);
+
+    int i;
+    for (i = 0; i < tam_buf; i++) {
+        if (
+            (i < tam_exp && buf[i] != exp_str[i]) ||
+            (i >= tam_exp && buf[i] != '\0')
+        ) return false;
+    }
+
+    return true;
+}
+
+
+// Le um registro de formato de saida padrão do arquivo
+Registro leRegistroStdoutFile(FILE *stdout_file) {
+    Registro registro = newRegistro();
+
+    fscanf(stdout_file, "%[^\n]s", registro.nome_autor);
+    fscanf(stdout_file, "%*c");
+    fscanf(stdout_file, "%[^\n]s", registro.titulo);
+    fscanf(stdout_file, " %d", &registro.ano);
+    fscanf(stdout_file, "%*c");
+    fscanf(stdout_file, "%[^\n]s", registro.nome_arquivo);
+    fscanf(stdout_file, "%*c");
+
+    return registro;
+}
+
+// Verifica se os buffers do registro lido contem lixo
+bool validaBuffersRegistro(Registro *registro_lido, Registro *registro_comparar) {
+    return assertBufferStr(registro_lido->nome_autor, TAMANHO_MAX_STRING + 1, registro_comparar->nome_autor) &&
+           assertBufferStr(registro_lido->nome_arquivo, TAMANHO_MAX_STRING + 1, registro_comparar->nome_arquivo) &&
+           assertBufferStr(registro_lido->titulo, TAMANHO_MAX_STRING + 1, registro_comparar->titulo);
 }
--- lib/tests/test_lib.h
+++ lib/tests/test_lib.h
@@ -1,7 +1,11 @@
 #ifndef TEST_LIB_H
 #define TEST_LIB_H
 
+#include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+#include "../registro.h"
 
 /**
  * Inicializa um arquivo de teste, setando algumas propriedades necessárias para o correto funcionamento
@@ -27,5 +31,33 @@ void copyStrToBuffer(char *dest, const char *ori, size_t tam);
  * @param tam Tamanho do buffer
  */
 void cleanBuffer(char *dest, int tam);
+
+/**
+ * Verifica se a string possui caracteres estranhos após o \0
+ *
+ * @param buf Buffer ou string a ser validada
+ * @param tam_buf tamanho do buffer
+ * @param exp_str String a ser comparada
+ * @return Retorna true se a string comparada é válida e não possui caracteres estranhos após o final
+ */
+bool assertBufferStr(char *buf, size_t tam_buf, const char *exp_str);
+
+
+/**
+ * Le um registro de formato de saida padrão do arquivo
+ *
+ * @param stdout_file Arquivo de saída
+ * @return Retorna um registro obtido através da impressão num arquivo
+ */
+Registro leRegistroStdoutFile(FILE *stdout_file);
+
+/**
+ * Verifica se os buffers do registro lido contem lixo
+ *
+ * @param registro_lido Registro lido do arquivo
+ * @param registro_comparar Registro a ser comparado
+ * @return True se os buffers do arquivo são semelhantes ao registro e não contém lixo
+ */
+bool validaBuffersRegistro(Registro *registro_lido, Registro *registro_comparar);
 
 #endif //TEST_LIB_H
